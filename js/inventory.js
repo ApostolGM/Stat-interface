1ed8ba6b4a050ff461acb63d5134d0eeb6fb8552
ApostolGM <wetsrew@gmail.com>
2026-06-07 18:53:26 +0300

    Create inventory.js

---
+++ js/inventory.js
@@ -0,0 +1,16 @@
+// inventory.js
+import { getInventory } from './ui.js';
+
+export function renderInventory() {
+    const inv = getInventory();
+    if (inv.length === 0) {
+        document.getElementById('inventoryContent').innerHTML = '<p style="text-align:center;">ИНВЕНТАРЬ ПУСТ</p>';
+    } else {
+        let html = '<ul style="list-style:none; padding:0;">';
+        inv.forEach(item => {
+            html += `<li style="padding:5px 0; border-bottom:1px solid #1a3a1a;">${item}</li>`;
+        });
+        html += '</ul>';
+        document.getElementById('inventoryContent').innerHTML = html;
+    }
+}
