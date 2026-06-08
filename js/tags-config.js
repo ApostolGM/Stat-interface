0257a175d2a3ce7eda98241f70a23011b1033f08
ApostolGM <wetsrew@gmail.com>
2026-06-08 07:08:14 +0300

    Create tags-config.js

---
+++ js/tags-config.js
@@ -0,0 +1,32 @@
+// tags-config.js
+import { db } from './firebase-config.js';
+import { doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+export function subscribeToTags(callback) {
+    const docRef = doc(db, "config", "tags");
+    return onSnapshot(docRef, (docSnap) => {
+        if (docSnap.exists()) {
+            callback(docSnap.data().tags || []);
+        } else {
+            const defaultTags = [
+                { id: "weapon", name: "Оружие", color: "#FF4444" },
+                { id: "armor", name: "Броня", color: "#4444FF" },
+                { id: "med", name: "Медицина", color: "#44FF44" },
+                { id: "food", name: "Еда", color: "#FFAA44" },
+                { id: "ammo", name: "Патроны", color: "#FF8844" },
+                { id: "common", name: "Обычное", color: "#888888" },
+                { id: "rare", name: "Редкое", color: "#4488FF" },
+                { id: "legendary", name: "Легендарное", color: "#FFAA00" },
+                { id: "bestiary", name: "Бестиарий", color: "#FF44FF" },
+                { id: "unique", name: "Уникальное", color: "#FF4488" }
+            ];
+            setDoc(docRef, { tags: defaultTags });
+            callback(defaultTags);
+        }
+    });
+}
+
+export async function updateTags(tags) {
+    const docRef = doc(db, "config", "tags");
+    await setDoc(docRef, { tags });
+}
