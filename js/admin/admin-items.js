3733200bdc5985eaf9d1345b417477a24dc9d9ed
ApostolGM <wetsrew@gmail.com>
2026-06-08 09:12:44 +0300

    Create admin-items.js

---
+++ js/admin/admin-items.js
@@ -0,0 +1,7 @@
+// admin/admin-items.js (заглушка)
+import { log } from '../shared.js';
+
+export function renderItemsAdmin() {
+    const inner = document.getElementById('adminPanelContent');
+    inner.innerHTML = '<h3>БИБЛИОТЕКА ПРЕДМЕТОВ</h3><p>Модуль в разработке</p>';
+}
