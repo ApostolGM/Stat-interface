c696caab659e86d97c28e4bb8a518cc7bbfa66e8
ApostolGM <wetsrew@gmail.com>
2026-06-08 14:28:22 +0300

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,4 +1,3 @@
-// db.js
 import { db } from './firebase-config.js';
 import { doc, onSnapshot, updateDoc } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 import { setCurrencies, setInventory } from './state.js';
@@ -8,10 +7,8 @@ export function subscribeToUserData(userId, callback) {
     return onSnapshot(userDocRef, (docSnap) => {
         if (docSnap.exists()) {
             const data = docSnap.data();
-            // Обновляем локальное состояние
             setCurrencies(data.currencies || { pink: 0, gray: 0, yellow: 0 });
             setInventory(data.inventory || []);
-            // Вызываем колбэк с данными
             if (callback) callback(data);
         }
     });
