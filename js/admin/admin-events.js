c1954550f8c15057ac1d6589192056866749420b
ApostolGM <wetsrew@gmail.com>
2026-06-08 09:12:20 +0300

    Create admin-events.js

---
+++ js/admin/admin-events.js
@@ -0,0 +1,7 @@
+// admin/admin-events.js (заглушка)
+import { log } from '../shared.js';
+
+export function renderEventsAdmin() {
+    const inner = document.getElementById('adminPanelContent');
+    inner.innerHTML = '<h3>СОБЫТИЯ</h3><p>Модуль в разработке</p>';
+}
