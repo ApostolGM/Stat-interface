9ac632878294cd02b32cf4ddb633e95dc48440a6
ApostolGM <wetsrew@gmail.com>
2026-06-08 14:30:34 +0300

    Update items-config.js

--- js/items-config.js
+++ js/items-config.js
@@ -1,22 +1,11 @@
-// items-config.js
 import { db } from './firebase-config.js';
 import { doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 export function subscribeToItems(callback) {
     const docRef = doc(db, "config", "items");
     return onSnapshot(docRef, (docSnap) => {
-        if (docSnap.exists()) {
-            callback(docSnap.data().items || []);
-        } else {
-            const defaultItems = [
-                { id: "item_water", name: "Фляга с водой", description: "", image: "", tags: ["food"], basePrice: 1 },
-                { id: "item_medkit", name: "Аптечка малая", description: "Восстанавливает здоровье", image: "", tags: ["med", "common"], basePrice: 3 },
-                { id: "item_pm", name: "ПМ", description: "Пистолет Макарова", image: "", tags: ["weapon", "common"], basePrice: 5, baseDurability: 100 },
-                { id: "item_bandage", name: "Бинт стерильный", description: "", image: "", tags: ["med", "common"], basePrice: 1 }
-            ];
-            setDoc(docRef, { items: defaultItems });
-            callback(defaultItems);
-        }
+        if (docSnap.exists()) callback(docSnap.data().items || []);
+        else { setDoc(docRef, { items: [] }); callback([]); }
     });
 }
 
