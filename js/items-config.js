d3c1c1c0597edee4d77617a68479a44bb43386b1
ApostolGM <wetsrew@gmail.com>
2026-06-08 18:56:41 +0300

    Update items-config.js

--- js/items-config.js
+++ js/items-config.js
@@ -1,11 +1,16 @@
+// items-config.js
 import { db } from './firebase-config.js';
 import { doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
 export function subscribeToItems(callback) {
     const docRef = doc(db, "config", "items");
     return onSnapshot(docRef, (docSnap) => {
-        if (docSnap.exists()) callback(docSnap.data().items || []);
-        else { setDoc(docRef, { items: [] }); callback([]); }
+        if (docSnap.exists()) {
+            callback(docSnap.data().items || []);
+        } else {
+            setDoc(docRef, { items: [] });
+            callback([]);
+        }
     });
 }
 
